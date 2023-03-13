27ffa33fe8159f8a6d682af85ed702198378f210
Riya Guha Roy <68594776+riyaguharoy@users.noreply.github.com>
2023-03-13 16:59:27 +0200

    Update streamlit_app.py
    menu added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit;
-streamlit.header("Breakfast Menu");
-streamlit.text("Bread");
-streamlit.text("Butter");
-streamlit.text("Milk");
-streamlit.text("Masala Tea");
-streamlit.text("Orange Juice");
-streamlit.text("Boiled Egg");
+streamlit.title('My parents new healthy breakfast);
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
+              
+                   
 
