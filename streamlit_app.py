8bf0f2a195e9cff5f0be3a15a0a44d7732e04201
Riya Guha Roy <68594776+riyaguharoy@users.noreply.github.com>
2023-03-14 03:06:37 +0200

    Update streamlit_app.py
    z

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,5 @@ fruit_selected=streamlit.multiselect("pick some fruits:", list(my_fruit_list.ind
 ##streamlit.dataframe(fruit_show);
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json)
