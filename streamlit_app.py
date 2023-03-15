ed8552254d6d7a2b411667209956334716699159
Riya Guha Roy <68594776+riyaguharoy@users.noreply.github.com>
2023-03-15 14:56:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,6 @@ my_fruit_list=my_fruit_list.set_index('Fruit');
 fruit_selected=streamlit.multiselect("pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']);
 ##fruit_show=my_fruit_list.loc[fruit_selected]
 ##streamlit.dataframe(fruit_show);
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
@@ -46,4 +45,3 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
-import snowflake.connector
